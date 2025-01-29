2b98d14c712bd2baad469f7d67d773a31592ca77
Remenod <remenod@gmail.com>
2025-01-29 18:44:25 +0200

    timezone set

--- ScheduleSpyBot/logger.py
+++ ScheduleSpyBot/logger.py
@@ -1,11 +1,12 @@
 from botBase import bot, logClient
 from enumerations import AdminPanel
 from datetime import datetime
+from pytz import timezone
 
-def log(text:str, threadId:int = AdminPanel.logerThreadId.value):
-    current_time = datetime.now().strftime("%H:%M:%S")
-    print(f"{current_time} - {text}")
+def log(text:str, threadId:int = AdminPanel.logerThreadId.value):    
     try:
+        current_time = datetime.now(timezone("Europe/Kyiv")).strftime("%H:%M:%S")
+        print(f"{current_time} - {text}")
         if(threadId == AdminPanel.logerThreadId.value):
             logClient.send_message(AdminPanel.groupId.value, text, message_thread_id=threadId)
         else:
