5b0d6f1660c9b746ae08d2329251e4f830706cc0
Remenod <remenod@gmail.com>
2025-01-29 18:23:05 +0200

    log time

--- ScheduleSpyBot/logger.py
+++ ScheduleSpyBot/logger.py
@@ -1,9 +1,11 @@
 from botBase import bot, logClient
 from enumerations import AdminPanel
+from datetime import datetime
 
 def log(text:str, threadId:int = AdminPanel.logerThreadId.value):
-    print(text)
-    try:        
+    current_time = datetime.now().strftime("%H:%M:%S")
+    print(f"{current_time} - {text}")
+    try:
         if(threadId == AdminPanel.logerThreadId.value):
             logClient.send_message(AdminPanel.groupId.value, text, message_thread_id=threadId)
         else:
