5d118e759aae19ed45791adda67d933fb88f3f28
Remenod <remenod@gmail.com>
2025-02-04 12:49:29 +0200

    logger interpolationless correct text view

--- ScheduleSpyBot/logger.py
+++ ScheduleSpyBot/logger.py
@@ -9,9 +9,9 @@ def log(text:str, threadId:int = AdminPanel.logerThreadId.value):
         current_time = datetime.now(timezone("Europe/Kyiv")).strftime("%H:%M:%S")
         print(f"{current_time} - {text}")
         if(threadId == AdminPanel.logerThreadId.value):
-            logClient.send_message(AdminPanel.groupId.value, text, message_thread_id=threadId)
+            logClient.send_message(AdminPanel.groupId.value, str(text), message_thread_id=threadId)
         else:
-            bot.send_message(AdminPanel.groupId.value, text, message_thread_id=threadId)
+            bot.send_message(AdminPanel.groupId.value, str(text), message_thread_id=threadId)
     except ApiException as r:
             if "retry after" in str(e):
                 log("Виникла помилка Логера. Забагато запитів.")
